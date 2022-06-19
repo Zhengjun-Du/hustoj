8328b622b30072b7c4dddc03b3b5e32730495c2c
AloneH <admin@oibit.cn>
2022-06-19 15:55:27 +0800

    fix: 未知变量错误

--- trunk/web/conteststatistics.php
+++ trunk/web/conteststatistics.php
@@ -10,7 +10,7 @@
 if (!isset($_GET['cid'])) die("No Such Contest!");
 $cid=intval($_GET['cid']);
 
-$sql="SELECT title,end_time FROM `contest` WHERE `contest_id`=? AND `start_time`<NOW()";
+$sql="SELECT title,end_time,start_time FROM `contest` WHERE `contest_id`=? AND `start_time`<NOW()";
 $result=pdo_query($sql,$cid);
 $num=count($result);
 if ($num==0){
@@ -21,6 +21,7 @@ if ($num==0){
 $row=$result[0];
 $title=$row[0];
 $end_time=strtotime($row[1]);
+$start_time=strtotime($row['start_time']);
 	$noip = (time()<$end_time) && (stripos($title,$OJ_NOIP_KEYWORD)!==false);
 	if(isset($_SESSION[$OJ_NAME.'_'."administrator"])||
 		isset($_SESSION[$OJ_NAME.'_'."m$cid"])||
